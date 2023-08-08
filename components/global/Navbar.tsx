8b6207fa38230893c7506a52d29f3198310e39b2
junaidhkn <junaidhkniazi@gmail.com>
2023-08-09 01:02:27 +0500

    upload thing

--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
 	return (
 		<React.Fragment>
 			<nav className='h-14 w-full flex justify-center items-center'>
-				<h1 className='text-4xl text-gray-800'>ShareME</h1>
+				<h1 className='text-4xl text-white/75 font-bold'>ShareME</h1>
 			</nav>
 		</React.Fragment>
 	);
--- components/global/Upload.tsx
+++ components/global/Upload.tsx
@@ -1,5 +1,19 @@
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
 const Upload = () => {
-	return <div>Upload</div>;
+	return (
+		<main className='h-[400px] bg-slate-400/80'>
+			<div className='grid w-full max-w-sm items-center gap-1.5'>
+				<Label htmlFor='picture'>File</Label>
+				<Input
+					id='picture'
+					type='file'
+				/>
+			</div>
+			<form></form>
+		</main>
+	);
 };
 
 export default Upload;
