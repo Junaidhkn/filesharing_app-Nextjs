379be560bc130ac97a9a276d1b64972d60effae3
junaidhkn <junaidhkniazi@gmail.com>
2023-08-08 23:56:12 +0500

    initial styling

---
+++ components/global/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className='flex justify-between fixed bottom-0 bg-slate-700 w-full h-9 items-center px-10'>
+			<p className=' text-gray-200'>
+				&copy; 2023-25 ShareMe. All rights reserved
+			</p>
+			<p className=' text-gray-200'>Made with ❤️ by Junaid Hassan Khan</p>
+		</footer>
+	);
+};
+
+export default Footer;
