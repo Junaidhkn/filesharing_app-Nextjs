0b80c20ff65ac8acb200af728d6904c10f703fdb
junaidhkn <junaidhkniazi@gmail.com>
2023-08-08 23:56:22 +0500

    initial

---
+++ components/global/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<React.Fragment>
+			<nav className='w-full'>
+				<h1 className='text-4xl text-gray-800'>ShareME</h1>
+			</nav>
+		</React.Fragment>
+	);
+};
+
+export default Navbar;
