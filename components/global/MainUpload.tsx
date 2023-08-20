cd34ed3703233ed7f1c7c2cd31dd123bd44a9728
junaidhkn <junaidhkniazi@gmail.com>
2023-08-20 17:07:16 +0500

    UI changes

--- components/global/MainUpload.tsx
+++ components/global/MainUpload.tsx
@@ -1,109 +1,89 @@
 'use client';
 
-import React, { useState } from 'react';
-import Upload from './Upload';
-
 const MainUpload = () => {
-	const [id, setId] = useState('');
-	const [name, setName] = useState('');
-	const [size, setsize] = useState();
-	const [show, setShow] = useState(false);
-	const [emailto, setEmailTo] = useState('');
-	const [emailfrom, setEmailFrom] = useState('');
-	const [title, setTitle] = useState('');
-	const [description, setDescription] = useState('');
-	const [data, setData] = useState();
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(false);
-	const [modalOpen, setModalOpen] = useState(false);
-
 	return (
-		<div className='flex justify-between items-center'>
-			<div className='border border-black basis-[20%]'>
-				<Upload />
-			</div>
-			<div className=''>
-				<form
-					// onSubmit=''
-					method='post'>
-					<div className=''>
+		<div className='p-9 w-[650px] h-full border border-black'>
+			<div className='flex gap-7'>
+				<div className='input basis-[40%]'>
+					<form
+						method='post'
+						encType='multipart/form-data'>
 						<input
-							className=''
-							type='email'
-							name='emailto'
-							id='emailto'
-							// value={emailto}
-							onChange={(e) => {
-								setEmailTo(e.target.value);
-							}}
-							required
+							id='file'
+							type='file'
+							name='file'
+							accept='*/*'
+							className='custom-uploader'
 						/>
-						<label
-							className='textField-label'
-							htmlFor='emailto'>
-							Email to :
-						</label>
-					</div>
-					<div className='input-imp'>
-						<input
-							className='textField-input'
-							type='email'
-							name='emailfrom'
-							id='emailfrom'
-							value={emailfrom}
-							onChange={(e) => {
-								setEmailFrom(e.target.value);
-							}}
-							required
-						/>
-						<label
-							className='textField-label'
-							htmlFor='emailfrom'>
-							Your Email :
-						</label>
-					</div>
-					<div className='input-imp'>
-						<input
-							className='textField-input'
-							type='text'
-							name='title'
-							id='title'
-							value={title}
-							onChange={(e) => {
-								setTitle(e.target.value);
-							}}
-							required
-						/>
-						<label
-							className='textField-label'
-							htmlFor='title'>
-							Title :
-						</label>
-					</div>
-					<div className='input-imp'>
-						<textarea
-							className='textField-input'
-							name='description'
-							id='description'
-							value={description}
-							onChange={(e) => {
-								setDescription(e.target.value);
-							}}
-							required></textarea>
-						<label
-							className='textField-label'
-							htmlFor='description'>
-							Message :
-						</label>
-					</div>
-					<div className='action-trigger'>
-						<button
-							type='submit'
-							className='action-button'>
-							Transfer
-						</button>
-					</div>
-				</form>
+					</form>
+				</div>
+				<div className='h-[350px] w-[0.5px] bg-slate-800'></div>
+				<div className='form-container basis-[70%] font-bold text-white'>
+					<form method='post'>
+						<div className='input-imp'>
+							<input
+								className='textField-input'
+								type='email'
+								name='emailto'
+								id='emailto'
+								required
+							/>
+							<label
+								className='textField-label'
+								htmlFor='emailto'>
+								Email to :
+							</label>
+						</div>
+						<div className='input-imp'>
+							<input
+								className='textField-input'
+								type='email'
+								name='emailfrom'
+								id='emailfrom'
+								required
+							/>
+							<label
+								className='textField-label'
+								htmlFor='emailfrom'>
+								Your Email :
+							</label>
+						</div>
+						<div className='input-imp'>
+							<input
+								className='textField-input'
+								type='text'
+								name='title'
+								id='title'
+								required
+							/>
+							<label
+								className='textField-label'
+								htmlFor='title'>
+								Title :
+							</label>
+						</div>
+						<div className='input-imp'>
+							<textarea
+								className='textField-input'
+								typeof='text'
+								name='description'
+								id='description'
+								required></textarea>
+							<label
+								className='textField-label'
+								htmlFor='description'>
+								Message :
+							</label>
+						</div>
+						<div className='action-trigger'>
+							<button
+								type='submit'
+								className='action-button'>
+								Transfer
+							</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
 	return (
 		<React.Fragment>
-			<nav className='h-14 w-full flex justify-center items-center'>
+			<nav className='h-14 w-full flex justify-center items-center bg-slate-500'>
 				<h1 className='text-4xl text-white/75 font-bold'>ShareME</h1>
 			</nav>
 		</React.Fragment>
